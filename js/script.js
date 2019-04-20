235875fa5b753928510bed36ebca1883de940982
Vishal-1999 <vishalsharma161999@gmail.com>
2019-04-20 21:01:27 +0530

    Survey-form complete

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const role = document.querySelector("#role");
+
+// role.addEventListener("change", ()=>{
+//     console.log(role.value);
+//     document.getElementById("root").textContent = role.value;
+// });
